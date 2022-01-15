d91bc99c91dee3a8b440c86f28e6d1fd1caab098
somerchen <somercxb@gmail.com>
2022-01-15 17:13:41 +0800

    feat: 添加侧边栏

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,4 +5,31 @@ export default defineConfig({
   description: 'notes',
   base: '/notes/',
   dest: 'dist',
+  themeConfig: {
+    lastUpdated: '上次更新',
+    editLinkText: '在 GitHub 上编辑此页',
+    // nav: [
+    //   { text: 'Home', link: '/' },
+    //   { text: 'Guide', link: '/guide/' },
+    //   { text: 'External', link: 'https://google.com' },
+    // ],
+    sidebar: [
+      {
+        title: '面试题',
+        path: '/面试题/',
+        collapsable: false,
+        children: [
+          '/面试题/HTML/',
+          '/面试题/CSS/',
+          '/面试题/JS/',
+          '/面试题/TS/',
+          '/面试题/React/',
+          '/面试题/Vue/',
+          '/面试题/Webpack/',
+          '/面试题/网络/',
+          '/面试题/手写系列/',
+        ]
+      },
+    ],
+  },
 });
